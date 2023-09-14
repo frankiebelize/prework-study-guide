01197909e8c95a3e1d94320f181ef68457dfa54c
frankiebelize <66229510+frankiebelize@users.noreply.github.com>
2023-09-13 23:06:38 -0500

    functions included

--- assets/script.js
+++ assets/script.js
@@ -23,7 +23,30 @@ console.log("Lets study",topic[3]);
 
 console.log("test code 2");
 */
-var topic = ["HTML","CSS","Git","JavaScript"];
+function listTopics(){
+
 for (var x=0; x < topic.length;x++){
-console.log("Lets study",topic[x]);
+console.log(topic[x]);
 }
+}
+
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+      } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+      } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+      } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+      } else {
+        console.log('Please try again!');
+      }
+      
+}
+var topic = ["HTML","CSS","Git","JavaScript"];
+var randomTopic = topic[Math.floor(Math.random() * topic.length)];
+console.log("Here are a list of topics.");
+listTopics()
+console.log("Which topic should we study first?");
+selectTopic()
