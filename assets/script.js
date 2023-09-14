f4015d8639b5f7ac0db41d49704db0af42788f3c
frankiebelize <66229510+frankiebelize@users.noreply.github.com>
2023-09-13 22:10:04 -0500

    added array with for loop

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+/*
+var topic = "HTML";
+
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+
+console.log("test code 2");
+
+var topic = ["HTML","CSS","Git","JavaScript"];
+console.log("Lets study",topic[0]);
+console.log("Lets study",topic[1]);
+console.log("Lets study",topic[2]);
+console.log("Lets study",topic[3]);
+
+console.log("test code 2");
+*/
+var topic = ["HTML","CSS","Git","JavaScript"];
+for (var x=0; x < topic.length;x++){
+console.log("Lets study",topic[x]);
+}
